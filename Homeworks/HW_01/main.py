747e7f2d8495d7adac53e91cabdc9d694417bf4c
Vlas_Mastykash <mastykash.vlas@ya.ru>
2021-09-07 04:35:21 +0600

    first python3 HW

---
+++ Homeworks/HW_01/main.py
@@ -0,0 +1,46 @@
+
+    # 1) Создать переменную типа String
+nameString = 'String'
+    # 2) Создать переменную типа Integer
+nameInteger = 12345
+    # 3) Создать переменную типа Float
+nameFloat = 123.45
+    # 4) Создать переменную типа Bytes
+nameBytes = bytes(nameInteger)
+    # 5) Создать переменную типа List
+nameList = [1, 2, 3, 4, 5]
+    # 6) Создать переменную типа Tuple
+nameTuple = ('Vlas',)
+nameTuple2 = tuple(nameList)
+    # 7) Создать переменную типа Set
+nameSet = {1, 2, 3, 3, 4, 4, 4, 5}
+nameSet2 = set(nameTuple2)
+    # 8. Создать переменную типа Frozen set
+nameFrozenSet = frozenset(nameSet)
+    # 9) Создать переменную типа Dict
+nameDict = {1:"dog", 2:"cat", 3:"mouse"}
+
+    # 10) Вывести в консоль все выше перечисленные переменные с добавлением типа данных.
+print(nameString, type(nameString))
+print(nameInteger, type(nameInteger))
+print(nameFloat, type(nameFloat))
+print(nameBytes, type(nameBytes))
+print(nameList, type(nameList))
+print(nameTuple, type(nameTuple))
+print(nameTuple2, type(nameTuple2))
+print(nameSet, type(nameSet))
+print(nameSet2, type(nameSet2))
+print(nameFrozenSet, type(nameFrozenSet))
+print(nameDict, type(nameDict))
+
+# 11) Создать 2 переменные String, создать переменную в которой суммируете эти переменные. Вывести в консоль.
+#
+# 12) Создать 2 переменные Integer, создать переменную в которой суммируете эти переменные. Вывести в консоль.
+#
+# 13) Создать переменную в которой Разделите эти переменные Int. Вывести в консоль.
+# 14) Создать переменную в которой Умножите эти переменные Int. Вывести в консоль.
+# 15) Создать переменную в которой Разделите без остатка эти переменные Int. Вывести в консоль.
+# 16) Создать переменную в которой надо присвоить остаток от деления этих переменные Int. Вывести в консоль.
+# 17) (7 + 12)  3 + 7 * 4 - 44 / 2  4 расставить точки так чтобы получилось 6884.25. Вывести в консоль.
+#
+# Выгрузить файл в Git репозиторий.
