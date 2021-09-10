96c473b5f8a4329b761e22a5d79e1e150e083b02
Vlas_Mastykash <mastykash.vlas@gmail.com>
2021-09-10 23:25:59 +0600

    "Lesson 3"

--- Python_Practice/pythonProject/main.py
+++ Python_Practice/pythonProject/main.py
@@ -1,57 +1,116 @@
-# Lesson 1
+# # Lesson 1
+#
+# print(1)    # вывод целого числа
+# print(2)
+# print('Vlas')   # вывод строки
+# a = 6     # обявление переменной а со значением 6
+# # Comment #1
+# if a > 3:
+#     print('a =', a)
+#     # todo add cycle
+# param = 1
+# print(str(param), 'Vlas', 'Влас', str(True), '*&#@^$')
+#
+# # while True:
+# #     name = input()
+# #     print('Your input word =', name)
+#
+# # Lesson 2
+#
+# b_true = True
+# b_false = False
+# v_1 = 3
+# v_2 = 5
+# print(v_1 == v_2)   # Равно
+# print(v_1 != v_2)   # Неравно
+# print(v_1 > v_2)
+# print(v_1 < v_2)
+# print(v_1 >= v_2)
+# print(v_1 <= v_2)
+#
+# age = 32
+# weight = 97
+# qa = True
+#
+# result = age > 30 and weight > 90   # and
+# print(result)
+# result = age == 32 and weight > 90
+# print(result)
+#
+# result = age > 30 or weight > 90   # or
+# print(result)
+# result = age == 32 or weight > 90
+# print(result)
+#
+# result = not age == 35   # not
+# print(result)
+#
+# if age == 32:
+#     print('Condition_1')
+#     if weight == 97:
+#         print('Condition_4')
+#     else:
+#         print('Condition_5')
+# elif age > 35:
+#     print('Condition_3')
+# else:
+#     print('Condition_2')
 
-print(1)    # вывод целого числа
-print(2)
-print('Vlas')   # вывод строки
-a = 6     # обявление переменной а со значением 6
-# Comment #1
-if a > 3:
-    print('a =', a)
-    # todo add cycle
-param = 1
-print(str(param), 'Vlas', 'Влас', str(True), '*&#@^$')
+# Lesson 3
 
+
+count = 1
 # while True:
-#     name = input()
-#     print('Your input word =', name)
+#     print("count =", count)
+#     count += 1
 
-# Lesson 2
+# while count < 100:
+#     print("count =", count)
+#     count += 1
 
-b_true = True
-b_false = False
-v_1 = 3
-v_2 = 5
-print(v_1 == v_2)   # Равно
-print(v_1 != v_2)   # Неравно
-print(v_1 > v_2)
-print(v_1 < v_2)
-print(v_1 >= v_2)
-print(v_1 <= v_2)
+# while count < 100:
+#     print("count =", count)
+#     count = count + 1
 
-age = 32
-weight = 97
-qa = True
+# while count <= 100:
+#     print("count =", count)
+#     count = count + 1
 
-result = age > 30 and weight > 90   # and
-print(result)
-result = age == 32 and weight > 90
-print(result)
+# while count <= 100:
+#     print("count =", count, count <= 100)
+#     if count > 50:
+#         print('--count', count)
+#     count = count + 1
 
-result = age > 30 or weight > 90   # or
-print(result)
-result = age == 32 or weight > 90
-print(result)
+# for i in range(0, 100):
+#     print('i =', i)
 
-result = not age == 35   # not
-print(result)
+# for i in range(0, 100, 5):  # шаг 5
+#     print('i =', i)
 
-if age == 32:
-    print('Condition_1')
-    if weight == 97:
-        print('Condition_4')
-    else:
-        print('Condition_5')
-elif age > 35:
-    print('Condition_3')
-else:
-    print('Condition_2')
+# for i in range(0, 100, 5):  # шаг 5
+#     print('count =', count, 'i = ', i)
+#     count += 1
+
+# test_list = [1, 2, 3, 4, 5, 6, 34, 72, 32, 14, 52, 92, 36, 57, 'ee', True, 4.2, [11, 22], {"item": 111}]
+# for i in test_list:
+#     print('count =', count, 'i = ', i)
+#     count += 1
+
+# test_list = [1, 2, 3, 4, 5, 6, 34, 72, 32, 14, 52, 92, 36, 57]
+# test_list_2 = []
+# print('test_list_2_before = ', test_list_2)
+# for i in test_list:
+#     print('count =', count, 'i = ', i)
+#     test_list_2.append(str(i + count) + ' years')
+#     count += 1
+# print('test_list_2_after = ', test_list_2)
+
+# test_list = [1, 2, 3, 4, 5, 6, 34, 72, 32, 14, 52, 92, 36, 57]
+# test_list_2 = []
+# print('test_list_2_before = ', test_list_2)
+# for i in test_list:
+#     print('count =', count, 'i = ', i)
+#     test_list_2.append(str(i + count) + ' years')
+#     count += 1
+# print('test_list_2_after = ', test_list_2)
