3682a0daab4a1f25d25842edab0a483749a26648
Vlas_Mastykash <mastykash.vlas@gmail.com>
2021-09-19 01:00:45 +0600

    acmp task903 done

--- Homeworks/HW_03/task_3.py
+++ Homeworks/HW_03/task_3.py
@@ -15,3 +15,15 @@
 #                 "Вы ввели пустое поле. Введите число." Если введено пустое значение.
 #     4. Валюту пользователя определите сами.
 
+your_money = float(input())
+convert_USD = your_money / 72.88
+convert_EUR = your_money / 85.46
+convert_CHF = your_money / 78.18
+convert_GBP = your_money / 100.14
+convert_CNY = your_money / 11.27
+print('Ты ввёл ', your_money, 'RUB')
+print('Конвертированная сумма в USD = ', round(convert_USD, 2))
+print('Конвертированная сумма в EUR = ', round(convert_EUR, 2))
+print('Конвертированная сумма в CHF = ', round(convert_CHF, 2))
+print('Конвертированная сумма в GBP = ', round(convert_GBP, 2))
+print('Конвертированная сумма в CNY = ', round(convert_CNY, 2))
--- README.md
+++ README.md
@@ -6,10 +6,21 @@ All Python projects I'll make I'll pull in this repo.
 ### Cheatsheet
 
 comm / func | discription
-:---:| ---
+ ---:| ---
+/ | всегда флоат
 ** | степень
 // | целочисленное деление
-% | остаток от деления
+% | взятие остатка от деления
+abs() | модуль числа
+min(), max() | min, max value of list
+pow(x,y) | возведение в степень
+round(x.abcde,3 OR -1) | математическое округление до тысячных OR десятков
+type() | тип объекта
+**0.5 OR **(1/2) OR pow(9,0.5)| квадратный корень, флоат
+a = b = c = 2 OR a, b, c = 1, 2, 3 | множественное присвоениезначений переменным
+input('Подсказка = ') | всегда str
+
+
 
 ### Operators priority
 - ()
