d51d0792e5d179d5f392e067c0adf47b93eab441
Vlas_Mastykash <mastykash.vlas@gmail.com>
2021-09-26 17:52:50 +0600

    hw3 json parse func

--- Homeworks/HW_03/task_4.py
+++ Homeworks/HW_03/task_4.py
@@ -22,27 +22,59 @@
 #             listRes.append((round((amount/['USDRUB'])*keys[i], 1)))
 #
 #     return list
-
-
+# input().upper()
+curr_input = 'CAD'
+amount_usd = 100
 jsonData = {"USDCAD": 1.227148, "USDCHF": 0.935689, "USDEUR": 0.837953, "USDGBP": 0.716673}
 keys = list(jsonData.keys())
 values = list(jsonData.values())
-jsonData.items()
-print(jsonData, keys, '|', values, '|', list(jsonData.items()))
-# def rate_currency(keys, amount):
-# num = {"USDCAD": 1.227148, "USDCHF": 0.935689, "USDEUR": 0.837953, "USDGBP": 0.716673}
-jsonData = {"USDCAD": 1.227148, "USDCHF": 0.935689, "USDEUR": 0.837953, "USDGBP": 0.716673}
-keys = jsonData.keys()
-values = jsonData.values()
 # jsonData.items()
-listRes = []
-# for i in keys:
-#     listRes.append((round((30/3['USDRUB'])*keys[i], 1)))
-#     print(listRes)
-amount = 1000
-currency = 'USD'+'CAD'
-item = ('USDCAD', 1.227148)
-# rate = item[currency]
-# rate = next(key for key in item if currency in item[key])
-rate = next(filter(lambda x: currency in item[x], item))
-print(rate)
+# print(jsonData, keys, values, list(jsonData.items()))
+# cad_string = keys[3:]
+# print(cad_string)
+# for currency in keys:
+#     if currency[3:] == curr_input:
+#         curr_index = keys.index(currency)
+#         print(curr_index)
+#         print(amount_usd * values[curr_index])
+def get_rate_value_from(parsed_json, currency_input):
+    currency_key_list = list(parsed_json.keys())
+    rate_values_list = list(parsed_json.values())
+    for currency in currency_key_list:
+        if currency[3:] == currency_input:
+            # curr_index = currency_key_list.index(currency)
+            rate_value = rate_values_list[currency_key_list.index(currency)]
+            return rate_value
+print(get_currency_from_keys(jsonData, curr_input))
+# def get_rate_from_values(parsed_json, currency_index):
+#     rate_values_list = list(parsed_json.values())
+
+
+
+# for
+# get_cad = amount_usd * values[0]
+# print(get_cad)
+# get_chf = amount_usd * values[1]
+# print(get_chf)
+# get_eur = amount_usd * values[2]
+# print(get_eur)
+# get_gbp = amount_usd * values[3]
+# print(get_gbp)
+
+# # def rate_currency(keys, amount):
+# # num = {"USDCAD": 1.227148, "USDCHF": 0.935689, "USDEUR": 0.837953, "USDGBP": 0.716673}
+# jsonData = {"USDCAD": 1.227148, "USDCHF": 0.935689, "USDEUR": 0.837953, "USDGBP": 0.716673}
+# keys = jsonData.keys()
+# values = jsonData.values()
+# # jsonData.items()
+# listRes = []
+# # for i in keys:
+# #     listRes.append((round((30/3['USDRUB'])*keys[i], 1)))
+# #     print(listRes)
+# amount = 1000
+# currency = 'USD'+'CAD'
+# item = ('USDCAD', 1.227148)
+# # rate = item[currency]
+# # rate = next(key for key in item if currency in item[key])
+# rate = next(filter(lambda x: currency in item[x], item))
+# print(rate)
--- Python_Practice/pythonProject3/main.py
+++ Python_Practice/pythonProject3/main.py
@@ -6,19 +6,20 @@ import time
 # count = 0
 # while True:
 #     count += 1
-#     if 100 < count <= 150:
+#     if 10 < count <= 30:
 #         time.sleep(.200)
 #         print('slow:', count)
 #         continue
 #     print('fast:', count)
-#     if count >= 200:
+#     if count >= 40:
 #         break
 
 # while True:
 #     input_data = float(input("Enter BYN money: "))
-#     if input_data < 0:
+#     if input_data <= 0:
 #         print('Enter number grater than 0: ')
 #         continue
+#     print('your amount', input_data)
 #     byn_to_usd = float(input_data) / 2.5
 #     print('USD = ', byn_to_usd)
 
@@ -35,25 +36,25 @@ import time
 # print('second_list', second_list)
 
 
-# def main():
-#     input_money = get_money()
-#     usd_money = exchange(input_money)
-#     print('usd_money =', usd_money)
-#
-#
-# def get_money():
-#     input_data = input('Enter UAH amount: ')
-#     return input_data
-#
-#
-# def exchange(money):
-#     int_money = int(money) / 27
-#     return int_money
-#
-#
-# main()
+def main():
+    input_money = get_money()
+    usd_money = exchange(input_money)
+    print('usd_money =', usd_money)
 
 
-from package_function.imp_func import main
+def get_money():
+    input_data = input('Enter UAH amount: ')
+    return input_data
+
+
+def exchange(money):
+    int_money = int(money) / 27
+    return int_money
+
 
 main()
+
+
+# from package_function.imp_func import main
+#
+# main()
