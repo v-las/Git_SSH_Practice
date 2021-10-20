0b47af4db16c632eb4cb2dd3626b04fc20ac0eca
Vlas_Mastykash <mastykash.vlas@gmail.com>
2021-10-20 06:01:14 +0600

    qa random interview task 01

--- Exercises/interview_tasks/iv_task_01/main.py
+++ Exercises/interview_tasks/iv_task_01/main.py
@@ -23,12 +23,12 @@ head_3_time = 20
 head_1_turns = ['r', 'f', 'b', 'l']
 head_2_turns = ['r', 'b', 'l']
 head_3_turns = ['f', 'r', 'l']
-# Отправляется в функцию время и стороны
+# Время и стороны отправляются в функцию
 head_1_workout = tick_count(head_1_turns, head_1_time)
 head_2_workout = tick_count(head_2_turns, head_2_time)
 head_3_workout = tick_count(head_3_turns, head_3_time)
-minute_list = []
 # Итерируется список для поиска совпадений
+minute_list = []
 for i in head_1_workout:
     # Очищается список от букв, приводится к int, чтобы получить список индексов
     i = int(i.translate({ord(i): None for i in 'fblr'}))
