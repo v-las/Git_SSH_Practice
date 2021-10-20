7754ca8b89b2cbd710616430ff69f28b75910326
Vlas_Mastykash <mastykash.vlas@gmail.com>
2021-10-20 15:04:56 +0600

    qa random interview task 01

--- Exercises/interview_tasks/iv_task_01/main.py
+++ Exercises/interview_tasks/iv_task_01/main.py
@@ -1,6 +1,6 @@
-def tick_count(turns, time):
+def tick_count(ticks, turns, time):
     """
-    Создаётся и итерируется список минут в трёх часах.
+    Создаётся и итерируется копия списка минут.
     К каждой минуте добавляется направление головы.
     При каждом времени поворота в списке направлений первый элемент
     перемещается в конец, чтобы добавлялся следующий.
@@ -8,30 +8,23 @@ def tick_count(turns, time):
     :param time: Время поворота.
     :return: Список минут с направлением головы в каждую минуту.
     """
-    ticks = [i for i in range(180)]
-    for i in ticks:
-        if ticks[i] // time == ticks[i] / time:
+    ticks_copy = ticks.copy()
+    for i in list(ticks_copy):
+        if ticks_copy[i] // time == ticks_copy[i] / time:
             turns.append(turns.pop(0))
-        ticks[i] = str(ticks[i]) + turns[0]
-    return ticks
+        ticks_copy[i] = str(ticks_copy[i]) + turns[0]
+    return ticks_copy
 
-# Время поворота каждой головы
-head_1_time = 10
-head_2_time = 15
-head_3_time = 20
-# Сторона направления головы
-head_1_turns = ['r', 'f', 'b', 'l']
-head_2_turns = ['r', 'b', 'l']
-head_3_turns = ['f', 'r', 'l']
-# Время и стороны отправляются в функцию
-head_1_workout = tick_count(head_1_turns, head_1_time)
-head_2_workout = tick_count(head_2_turns, head_2_time)
-head_3_workout = tick_count(head_3_turns, head_3_time)
+
+# Создаётся список минут в трёх часах
+head_ticks = [i for i in range(180)]
+# Для каждой головы: время поворота и направления отправляются в функцию
+head_1_workout = tick_count(head_ticks, ['r', 'f', 'b', 'l'], 10)
+head_2_workout = tick_count(head_ticks, ['r', 'b', 'l'], 15)
+head_3_workout = tick_count(head_ticks, ['f', 'r', 'l'], 20)
 # Итерируется список для поиска совпадений
 minute_list = []
-for i in head_1_workout:
-    # Очищается список от букв, приводится к int, чтобы получить список индексов
-    i = int(i.translate({ord(i): None for i in 'fblr'}))
+for i in head_ticks:
     # Сравниваются между собой списки трёх голов
     if head_1_workout[i] == head_2_workout[i] == head_3_workout[i]:
         # Совпадения (минуты с одинаковым направлением) добавляются в лист
--- Exercises/interview_tasks/iv_task_01/temp.py
+++ Exercises/interview_tasks/iv_task_01/temp.py
@@ -1,33 +1,49 @@
-tick_list_3 = [i for i in range(180)]
+def tick_count(turns, time):
+    """
+    Создаётся и итерируется список минут в трёх часах.
+    К каждой минуте добавляется направление головы.
+    При каждом времени поворота в списке направлений первый элемент
+    перемещается в конец, чтобы добавлялся следующий.
+    :param turns: Список направлений головы.
+    :param time: Время поворота.
+    :return: Список минут с направлением головы в каждую минуту.
+    """
+    ticks = [i for i in range(180)]
+    for i in ticks:
+            # print(ticks[i])
+        if ticks[i] // time == ticks[i] / time:
+            # print(ticks[i] // time)
+            # print(ticks[i] / time)
+            turns.append(turns.pop(0))
+        ticks[i] = str(ticks[i]) + turns[0]
+    return ticks
+
+
+# Время поворота каждой головы
+head_1_time = 10
+head_2_time = 15
+head_3_time = 20
+# Сторона направления головы
 head_1_turns = ['r', 'f', 'b', 'l']
 head_2_turns = ['r', 'b', 'l']
 head_3_turns = ['f', 'r', 'l']
+# Время и стороны отправляются в функцию
+head_1_workout = tick_count(head_1_turns, head_1_time)
+print(head_1_workout)
+head_2_workout = tick_count(head_2_turns, head_2_time)
+print(head_2_workout)
+head_3_workout = tick_count(head_3_turns, head_3_time)
+print(head_3_workout)
+# Итерируется список для поиска совпадений
+minute_list = []
+for i in head_1_workout:
+    # Очищается список от букв, приводится к int, чтобы получить список индексов
+    i = int(i.translate({ord(i): None for i in 'fblr'}))
+    # Сравниваются между собой списки трёх голов
+    if head_1_workout[i] == head_2_workout[i] == head_3_workout[i]:
+        # Совпадения (минуты с одинаковым направлением) добавляются в лист
+        minute_list.append(i)
+    print(minute_list)
+# Вывод в консоль подсчитанных элементов листа (минут)
+print("Три головы смотрели в одну сторону", len(minute_list), "минут.")
 
-tick_list_1 = [i for i in range(180)]
-for tick in tick_list_1:
-    if tick_list_1[tick] // 10 == tick_list_1[tick] / 10:
-        head_1_turns.append(head_1_turns.pop(0))
-    tick_list_1[tick] = str(tick_list_1[tick]) + head_1_turns[0]
-print(tick_list_1)
-
-tick_list_2 = [i for i in range(180)]
-for tick in tick_list_2:
-    if tick_list_2[tick] // 15 == tick_list_2[tick] / 15:
-        head_2_turns.append(head_2_turns.pop(0))
-    tick_list_2[tick] = str(tick_list_2[tick]) + head_2_turns[0]
-print(tick_list_2)
-
-tick_list_3 = [i for i in range(180)]
-for tick in tick_list_3:
-    if tick_list_3[tick] // 20 == tick_list_3[tick] / 20:
-        head_3_turns.append(head_3_turns.pop(0))
-    tick_list_3[tick] = str(tick_list_3[tick]) + head_3_turns[0]
-print(tick_list_3)
-
-new_list = []
-for i in tick_list_1:
-    i = int(i.translate({ord(i): None for i in 'frlb'}))
-    # print(i, type(i))
-    if tick_list_1[i] == tick_list_2[i] == tick_list_3[i]:
-        new_list.append(i)
-print(len(new_list))
