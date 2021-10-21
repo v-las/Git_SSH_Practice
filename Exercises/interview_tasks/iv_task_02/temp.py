bade4901593f6394cbcf6ea14a8d690ec6669727
Vlas_Mastykash <mastykash.vlas@gmail.com>
2021-10-22 00:41:46 +0600

    iv task 02 add temp

---
+++ Exercises/interview_tasks/iv_task_02/temp.py
@@ -0,0 +1,25 @@
+def path_count(graph, )
+
+
+for x in g:
+    for y in g.index(x):
+        mass = []
+        if y:
+            g[y]
+            break
+        else:
+            print(y)
+
+
+graph = {
+    'A': {'B', 'C', 'D', 'E'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+    'G':
+    'H':
+    'I':
+    'J':
+}
